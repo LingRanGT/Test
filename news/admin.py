007fafd628a2cd61188f6d7f0925c1ad5d4116ad
LingRan <17633811502@163.com>
2022-11-09 16:41:03 +0800

    新建新闻管理后台

---
+++ news/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import News
+
+# Register your models here.
+
+admin.site.site_header = '管理后台'  # 设置header
+admin.site.site_title = '管理后台'  # 设置title
+admin.site.index_title = '管理后台'
+
+
+@admin.register(News)
+class News(admin.ModelAdmin):
+    # 设置页面可以展示的字段
+    list_display = ('title', 'keyword', 'source', 'created_time', 'updated_time', 'pro_status')
+    # 默认不配置的话，第一个字段会存在链接到记录编辑页面
+    # list_display_links = None
+    list_display_links = ('title',)
+    # 设置过滤选项
+    list_filter = ('title', 'keyword', 'source', 'created_time', 'updated_time', 'pro_status')
+    # 设置可编辑字段 如果设置了可以编辑字段，页面会自动增加保存按钮
+    list_editable = ('keyword', 'source','pro_status')
+    # 每页显示条目数 缺省值100
+    list_per_page = 20
---
+++ news/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import django.utils.timezone as timezone
+
+# Create your models here.
+
+class News(models.Model):
+    title = models.CharField('标题', max_length=48, blank=False)
+    keyword = models.CharField('关键字', max_length=16, blank=False)
+    source = models.CharField('来源', max_length=16, blank=False)
+    content = models.TextField('内容')
+    created_time = models.DateTimeField('创建时间', default=timezone.now)  # 创建时间
+    updated_time = models.DateTimeField('更新时间', default=timezone.now)  # 更新时间
+    pro_status = models.CharField('发布状态', max_length=1, default='N')  # 发布状态 Y:发布；N:开发
+
+    class Meta:
+        verbose_name = "新闻列表"
+        verbose_name_plural = "新闻列表"
+
+    def __str__(self):
+        return self.title
